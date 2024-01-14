4cbb271af246e02296f9a958a69751223717431a
José Alexandre Cordeiro Souza <alexandrecordeiro15@outlook.com>
2024-01-14 07:58:58 -0300

    req 1

--- tests/ingredient/test_ingredient.py
+++ tests/ingredient/test_ingredient.py
@@ -1,6 +1,22 @@
-from src.models.ingredient import Ingredient  # noqa: F401, E261, E501
+from src.models.ingredient import Ingredient, Restriction  # noqa: F401, E261, E501
 
 
 # Req 1
 def test_ingredient():
-    pass
+    ingredient1 = Ingredient('queijo mussarela')
+    ingredient2 = Ingredient('carne')
+    ingredient3 = Ingredient('queijo mussarela')
+
+    assert ingredient1.name == "queijo mussarela"
+    assert ingredient1.restrictions == {
+        Restriction.LACTOSE,
+        Restriction.ANIMAL_DERIVED
+    }
+
+    assert ingredient1 == ingredient3
+    assert ingredient1 != ingredient2
+
+    assert hash(ingredient1) == hash(ingredient3)
+    assert hash(ingredient1) != hash(ingredient2)
+
+    assert repr(ingredient2) == "Ingredient('carne')"
